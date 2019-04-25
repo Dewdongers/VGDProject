bee029df637a88d7d7a9277650799b935291b30c
Dewdongers <Xenocrewgaming@gmail.com>
2019-04-25 09:14:21 -0500

    First Commit
    Bunch of nonsense to setup proper formatting

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+    public GameObject p;
+    public int speed;
+    private Vector3 move = new Vector3(0,0,0);
+
+    // Use this for initialization
+    void Start () {	
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+        Aim();
+        Move();
+	}
+
+    void Aim() {
+        Vector3 aimDir = Input.mousePosition;
+        p.transform.eulerAngles = new Vector3(0 , 0, (Mathf.Atan(aimDir.y / aimDir.x)/Mathf.PI)*180);
+    }
+    void Move()
+    {
+
+        if(Input.GetKeyDown("a")) {
+            move.x -= speed;
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            move.x += speed;
+        }
+
+
+        if (Input.GetKeyDown("w"))
+        {
+            move.y += speed;
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            move.y -= speed;
+        }
+        p.transform.localPosition = move;
+    }
+}
+
