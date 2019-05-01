6ecad059e2e6ab56397926ab1655d46e4162f60b
Dewdongers <Xenocrewgaming@gmail.com>
2019-05-01 09:39:50 -0500

    3w02a
    Tree and player colliders

--- Assets/Scripts/TreeGen.cs
+++ Assets/Scripts/TreeGen.cs
@@ -13,11 +13,14 @@ public class TreeGen : MonoBehaviour {
     public float y2;
 
 	void Start () {
-        for (int i = 0; i < 100 * Density; i++)
+        for (int i = 0; i < 20 * Density; i++)
         {
             Vector3 trans = new Vector3(rng.Next((int)x1, (int)x2),rng.Next((int)y1, (int)y2),0);
-            Quaternion rot = new Quaternion(0, 0, rng.Next(0, 360), 0);
-            Instantiate(tree,trans,rot);
+            Quaternion rot = new Quaternion(0, 0, rng.Next(0, 360), 180);
+            GameObject temp = Instantiate(tree,trans,rot);
+            float j = (float)(rng.NextDouble() * 16);
+            temp.transform.localScale = new Vector3(j,j,j);
+
         }
 	}
 }
