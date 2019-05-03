79bd21a753e18c094c93373843023bc27e056dfe
Dewdongers <201250@student.ponderisd.net>
2019-05-03 09:39:22 -0500

    3w06b
    tree opacity

---
+++ Assets/Scripts/TreeOpacity.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeOpacity : MonoBehaviour {
+    public float MaxOpacity;
+    public float MinOpacity;
+
+    public GameObject player;
+    private Vector2 Dir;
+    private float dist;
+    private float distx;
+    private float disty;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = player.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        distx = rb.position.x - transform.position.x;
+        disty = rb.position.y - transform.position.y;
+        dist = Mathf.Sqrt(Mathf.Pow(distx,2)+Mathf.Pow(disty,2));
+
+        transform.renderer
+    }
+}
