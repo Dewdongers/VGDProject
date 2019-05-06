9872938ea011ae94909b0347e24e2e061bf9c2e1
Dewdongers <Xenocrewgaming@gmail.com>
2019-05-06 09:18:21 -0500

    4w01a
    yee

--- Assets/Scripts/TreeOpacity.cs
+++ Assets/Scripts/TreeOpacity.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TreeOpacity : MonoBehaviour {
+public class TreeOpacity : MonoBehaviour
+{
     public float MaxOpacity;
     public float MinOpacity;
 
-    public GameObject player;
+    public GameObject tree;
+    private Renderer rend;
+    private GameObject player;
     private Vector2 Dir;
     private float dist;
     private float distx;
@@ -15,15 +18,17 @@ public class TreeOpacity : MonoBehaviour {
 
     private void Start()
     {
+        player = GameObject.Find("Player");
         rb = player.GetComponent<Rigidbody2D>();
+        rend = tree.GetComponent<Renderer>();
+
     }
 
     private void Update()
     {
-        distx = rb.position.x - transform.position.x;
-        disty = rb.position.y - transform.position.y;
+        distx = rb.position.x - tree.GetComponent<Rigidbody2D>().position.x;
+        disty = rb.position.y - tree.GetComponent<Rigidbody2D>().position.y;
         dist = Mathf.Sqrt(Mathf.Pow(distx,2)+Mathf.Pow(disty,2));
-
-        transform.renderer
+        rend.material.color = new Color(1.0f, 1.0f, 1.0f, dist);
     }
 }
