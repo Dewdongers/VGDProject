1f1f6233b50dc1e4500923c61eb5b4d785fdf31e
Dewdongers <Xenocrewgaming@gmail.com>
2019-05-07 09:01:12 -0500

    4w02a

--- Assets/Editor/FieldOfViewEditor copy.cs
+++ Assets/Editor/FieldOfViewEditor copy.cs
@@ -1,30 +1,30 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEditor;
+﻿//using UnityEngine;
+//using System.Collections;
+//using UnityEditor;
 
-[CustomEditor (typeof (FieldOfView))]
-public class FieldOfViewEditor : Editor {
-	void OnSceneGUI() {
-        FieldOfView fow = (FieldOfView)target; 
-        float rot = fow.rb.transform.rotation.z * 180;
-        Handles.color = Color.white;
-		Handles.DrawWireArc (fow.transform.position, Vector3.up, Vector3.forward, 360 , fow.viewRadius);
-        Vector2 viewAngleA = fow.DirFromAngle((-fow.viewAngle / 2) + rot);
-		Vector2 viewAngleB = fow.DirFromAngle((fow.viewAngle / 2) + rot);
-        Vector2 lookDir = fow.DirFromAngle(rot);
+//[CustomEditor (typeof (FieldOfView))]
+//public class FieldOfViewEditor : Editor {
+//	void OnSceneGUI() {
+//        FieldOfView fow = (FieldOfView)target; 
+//        float rot = fow.rb.transform.rotation.z * 180;
+//        Handles.color = Color.white;
+//		Handles.DrawWireArc (fow.transform.position, Vector3.up, Vector3.forward, 360 , fow.viewRadius);
+//        Vector2 viewAngleA = fow.DirFromAngle((-fow.viewAngle / 2) + rot);
+//		Vector2 viewAngleB = fow.DirFromAngle((fow.viewAngle / 2) + rot);
+//        Vector2 lookDir = fow.DirFromAngle(rot);
 
-		Handles.DrawLine (fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
-		Handles.DrawLine (fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
-        Handles.DrawLine (fow.transform.position, fow.transform.position + lookDir * fow.viewRadius);
-
-
+//		Handles.DrawLine (fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+//		Handles.DrawLine (fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+//        Handles.DrawLine (fow.transform.position, fow.transform.position + lookDir * fow.viewRadius);
 
 
-        Handles.color = Color.red;
 
-		foreach (Transform visibleTarget in fow.visibleTargets) {
-			Handles.DrawLine (fow.transform.position, visibleTarget.position);
-		}
-	}
 
-}
+//        Handles.color = Color.red;
+
+//		foreach (Transform visibleTarget in fow.visibleTargets) {
+//			Handles.DrawLine (fow.transform.position, visibleTarget.position);
+//		}
+//	}
+
+//}
--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -1,28 +1,28 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
+﻿//using UnityEngine;
+//using System.Collections.Generic;
 
-public class FieldOfView : MonoBehaviour
-{
-    public int viewRadius;
-    [Range(0, 360)]
-    public int viewAngle;
+//public class FieldOfView : MonoBehaviour
+//{
+//    public int viewRadius;
+//    [Range(0, 360)]
+//    public int viewAngle;
 
-    public int castResolution;
+//    public int castResolution;
 
-    public GameObject player;
-    private GameObject[] targets;
-    private Ray2D[] viewRays;
+//    public GameObject player;
+//    private GameObject[] targets;
+//    private Ray2D[] viewRays;
 
-    public void Start()
-    {
-        viewRays = new Ray2D[castResolution * 360];
-    }
+//    public void Start()
+//    {
+//        viewRays = new Ray2D[castResolution * 360];
+//    }
 
-    public void Update()
-    {
-        for (int i = 0; i < castResolution * 360; i++)
-        {
-            viewRays[i] = Ray2D();
-        }
-    }
-}
+//    public void Update()
+//    {
+//        for (int i = 0; i < castResolution * 360; i++)
+//        {
+//            viewRays[i] = Ray2D();
+//        }
+//    }
+//}
