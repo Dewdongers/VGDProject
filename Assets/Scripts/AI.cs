78eb866e0c1b2d338938cb713064a27481566a20
Dewdongers <Xenocrewgaming@gmail.com>
2019-05-13 09:39:08 -0500

    5w01a

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour {
+    GameObject Player;
+    Vector2 Targ;
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        Targ = new Vector2(transform.position.x - Player.transform.position.x, transform.position.y - Player.transform.position.y);
+    }
+
+    void Update () {
+        Targ.x = transform.position.x - Player.transform.position.x;
+        Targ.y = transform.position.y - Player.transform.position.y;
+        transform.rotation.Set(transform.rotation.x, transform.rotation.y, Mathf.Tan(Targ.y/Targ.x), 1);
+	}
+}
